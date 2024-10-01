db76d8a388c13960b4a3566bb82afffc93beee8c
fedtoscano <federicotoscano611@gmail.com>
2024-10-01 10:40:23 +0200

    created Model entity

---
+++ app/Models/Moment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Moment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name', 'message', 'started_at', 'ended_at', 'moment_img'
+    ];
+}
---
+++ database/migrations/2024_10_01_083405_create_moments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('moments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('message')->nullable();
+            $table->dateTime('started_at')->nullable();
+            $table->dateTime('ended_at')->nullable();
+            $table->string('moment_img')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('moments');
+    }
+};
