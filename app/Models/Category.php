1d1f6022b898cb717d9dfd933e8745fbd758e795
fedtoscano <federicotoscano611@gmail.com>
2024-10-01 11:24:49 +0200

    added category_id to tasks table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,8 @@ class Category extends Model
     protected $fillable = [
         'name', 'color', 'description'
     ];
+
+    public function tasks(){
+        return $this->belongsToMany(Task::class);
+    }
 }
