ee53cea2ef1365a0261b082379e4518b3328a238
GencoMattia <gencomattia@hotmail.it>
2024-10-08 16:23:40 +0200

    updated ApiUserController for authentication with JWT

--- app/Http/Controllers/Api/ApiUserController.php
+++ app/Http/Controllers/Api/ApiUserController.php
@@ -9,9 +9,17 @@ use Illuminate\Http\Request;
 
 class ApiUserController extends Controller
 {
-    public function create(CreateUserRequest $request)
-    {
+    /**
+     * Require JWT authentication for all methods except for user creation (store).
+     */
+    public function __construct() {
+        $this->middleware("auth:api", ["except" => ["store"]]);
+    }
+
+
+    public function store(CreateUserRequest $request) {
         $validatedData = $request->validated();
+
         $user = User::create([
             'name' => $validatedData['name'],
             'surname' => $validatedData['surname'],
@@ -22,5 +30,20 @@ class ApiUserController extends Controller
         return response()->json([
             'message' => 'User created successfully',
         ], 201);
+    }
+
+    public function show() {
+        $user = auth()->user();
+
+        return response()->json([
+            "message" => "User profile retrieved successfully",
+            "user" => $user,
+        ]);
+    }
+
+    public function update() {
+        $user = auth()->user();
+
+
     }
 }
