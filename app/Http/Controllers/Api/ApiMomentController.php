31bdb304db8425f75f721e4d34852df7a06011e2
fedtoscano <federicotoscano611@gmail.com>
2024-10-16 16:30:40 +0200

    created getFormData on ApiMomentController

--- app/Http/Controllers/Api/ApiMomentController.php
+++ app/Http/Controllers/Api/ApiMomentController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Emotion;
+use App\Models\MomentsType;
 use Illuminate\Http\Request;
 
 class ApiMomentController extends Controller
 {
-    //
+    public function getFormData(){
+        $moment_types = MomentsType::all();
+        $emotions = Emotion::all();
+
+        return response()->json([
+            'message'=>'success',
+            'data'=>[
+                'moment_types'=>$moment_types,
+                'emotions'=>$emotions
+            ]
+        ]);
+    }
 }
